e58a77c78f0cb262c5f70c971abaa8b6b1d5c04f
GeofferyThornock <geofferythornock@gmail.com>
2023-05-01 11:32:32 -0500

    learning about promises

--- src/main.js
+++ src/main.js
@@ -2,4 +2,14 @@ const { welcome, goodbye, tell } = require("../utils/fortune-teller");
 
 const promise = welcome();
 
-console.log(promise);
+const question = "Will the weather be nice today?";
+const tellPromise = tell(question);
+
+promise.then(console.log).then(goodbye).then(console.log).catch(console.error);
+
+tellPromise
+    .then((str) => {
+        console.log("Fortune Teller,", question);
+        console.log("Let me see", str);
+    })
+    .catch(console.error);
